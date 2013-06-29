565eaafa78a50e9c692e25c6a31336d808c544fd
kaosb <felipe.gonzalez.g@gmail.com>
2013-06-28 22:14:43 -0400

    n

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
+require 'rubygems'
+require 'bundler/setup'
 
 # Initialize the rails application
 Reporte::Application.initialize!
