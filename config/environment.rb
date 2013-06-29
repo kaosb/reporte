a95e42c31e011cdc32be0bcf7859207a63fbcdbd
kaosb <felipe.gonzalez.g@gmail.com>
2013-06-28 22:05:13 -0400

    Bleeee

--- config/environment.rb
+++ config/environment.rb
@@ -3,8 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Reporte::Application.initialize!
-
-require 'rubygems'
-require 'rubygems/gem_runner'
-ENV['GEM_PATH'] = '/home/trancar1/ruby/gems'
-Gem.clear_paths
