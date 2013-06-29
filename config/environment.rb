974fb9ee9646b063a6148dc93dd9ee2865270d7d
kaosb <felipe.gonzalez.g@gmail.com>
2013-06-29 10:40:31 -0400

    Ultimos cambios

--- config/environment.rb
+++ config/environment.rb
@@ -1,13 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-Gem.clear_paths
-ENV['GEM_HOME'] = '/home/trancar1/ruby/gems'
-ENV['GEM_PATH'] = File.expand_path('~/.rbenv/bin') + ':~/.rbenv/shims'
- 
-require 'rubygems'
-require 'bundler'
- 
-Bundler.require
 
 # Initialize the rails application
 Reporte::Application.initialize!
