fa18cec96df2dc120811afbfe0597e4a71baf7ee
kaosb <felipe.gonzalez.g@gmail.com>
2013-06-30 19:40:55 -0400

    Fix front

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,9 +5,10 @@ class HomeController < ApplicationController
 
 	def database
 		# Conectamos la BD
-		my = Mysql::new("127.0.0.1", "root", "kaosbite", "clerk_development")
+		#my = Mysql::new("127.0.0.1", "root", "kaosbite", "clerk_development")
+		my = Mysql::new("coddea.com", "trancar1_clinica", "clinica2012+-", "trancar1_calemana_deporteesmipasion2013")
 		# Contamos los registros en la BD
-		count = my.query("SELECT COUNT(*) FROM users USE INDEX(PRIMARY);")
+		count = my.query("SELECT COUNT(*) FROM participantes USE INDEX(PRIMARY);")
 		@cantidad = 0
 		count.each do |contador|
 			@cantidad = contador[0].to_i
@@ -19,18 +20,19 @@ class HomeController < ApplicationController
 		if !params[:page].blank?
 			page = params[:page].to_i * 20
 		end
-		@res = my.query("select * from users limit "+page.to_s+",20")
+		@res = my.query("select * from participantes limit "+page.to_s+",20")
 	end
 
 	def download
 		p = Axlsx::Package.new
 		# Conectamos la BD
-		my = Mysql::new("127.0.0.1", "root", "kaosbite", "clerk_development")
-		@res = my.query("select * from users")
+		#my = Mysql::new("127.0.0.1", "root", "kaosbite", "clerk_development")
+		my = Mysql::new("coddea.com", "trancar1_clinica", "clinica2012+-", "trancar1_calemana_deporteesmipasion2013")
+		@res = my.query("select * from participantes")
 		p.workbook.add_worksheet(:name => "Reporte") do |sheet|
 			sheet.add_row ["ID", "Nombre", "Apellido", "Nombre Completo", "Deporte", "Email", "Username", "Origen", "Cuando"]
 			@res.each do |elemento|
-				sheet.add_row [elemento[0], elemento[1], elemento[2], elemento[3], elemento[4], elemento[5], elemento[6], elemento[7], elemento[8]]
+				sheet.add_row [elemento[0], elemento[3], elemento[4], elemento[5], elemento[6], elemento[7], elemento[8], elemento[10], elemento[11]]
 			end
 		end
 		p.use_shared_strings = true
