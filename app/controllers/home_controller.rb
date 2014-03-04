3e5a42c4f18518be2f6260e0d4f6e901f22c74ae
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2014-03-03 21:21:50 -0300

    Dia de la mujer 2014

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
 	def database
 		# Conectamos la BD
 		#my = Mysql::new("127.0.0.1", "root", "kaosbite", "clerk_development")
-		my = Mysql::new("coddea.com", "trancar1_clinica", "clinica2012+-", "trancar1_calemana_navidad2013")
+		my = Mysql::new("coddea.com", "trancar1_clinica", "clinica2012+-", "trancar1_calemana_diadelamujer2014")
 		# Contamos los registros en la BD
 		my.query("SET NAMES UTF8")
 		count = my.query("SELECT COUNT(*) FROM participantes USE INDEX(PRIMARY);")
@@ -33,7 +33,7 @@ class HomeController < ApplicationController
 	def download
 		p = Axlsx::Package.new
 		# Conectamos la BD
-		my = Mysql::new("coddea.com", "trancar1_clinica", "clinica2012+-", "trancar1_calemana_navidad2013")
+		my = Mysql::new("coddea.com", "trancar1_clinica", "clinica2012+-", "trancar1_calemana_diadelamujer2014")
 		my.query("SET NAMES UTF8")
 		@res = my.query("select * from participantes")
 		# Comenzamos a aplicar estilos para axlsx
@@ -47,9 +47,9 @@ class HomeController < ApplicationController
 					:vertical => :center ,
 					:wrap_text => true }
 			p.workbook.add_worksheet(:name => "Reporte") do |sheet|
-				sheet.add_row ["fb_id", "Nombre", "Apellido", "Nombre Completo", "Email_fb", "Email actualizado", "Username", "Origen", "Cuando"], :style => wrap_text
+				sheet.add_row ["fb_id", "Nombre", "Apellido", "Nombre Completo", "Email", "Username", "Origen", "Cuando"], :style => wrap_text
 				@res.each do |elemento|
-					sheet.add_row [elemento[1], elemento[3], elemento[4], elemento[5], elemento[11], elemento[12], elemento[13], elemento[15], elemento[16]]
+					sheet.add_row [elemento[1], elemento[3], elemento[4], elemento[5], elemento[6], elemento[7], elemento[9], elemento[10]]
 				end
 			end
 		end
